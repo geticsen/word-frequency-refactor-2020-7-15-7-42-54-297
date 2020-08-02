85e0db15deaed2eb626feb19568a50515dc467eb
geticsen <286348794@qq.com>
2020-08-02 21:05:55 +0800

    [refactor] [Green] refactor: extract LINE_DELIMITER

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class WordFrequencyGame {
 
     public static final String WORD_DELIMITER = "\\s+";
+    public static final String LINE_DELIMITER = "\n";
 
     public String getResult(String inputStr) {
 
@@ -31,7 +32,7 @@ public class WordFrequencyGame {
 
                 inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
-                StringJoiner joiner = new StringJoiner("\n");
+                StringJoiner joiner = new StringJoiner(LINE_DELIMITER);
                 for (Input w : inputList) {
                     String s = w.getValue() + " " + w.getWordCount();
                     joiner.add(s);
