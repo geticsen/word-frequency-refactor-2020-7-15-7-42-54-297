e537ec8310345c9ffce9332580924c0415f8160e
geticsen <286348794@qq.com>
2020-08-02 21:44:49 +0800

    [refactor] [Green] refactor: delete unused if

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -9,34 +9,29 @@ public class WordFrequencyGame {
 
     public String getResult(String sentense) {
 
-        if (sentense.split(WORD_DELIMITER).length == 1) {
-            return sentense + " 1";
-        } else {
+        try {
 
-            try {
+            List<String> words = Arrays.asList(sentense.split(WORD_DELIMITER));
+            List<WordInfo> wordInfos = new ArrayList<>();
 
-                List<String> words = Arrays.asList(sentense.split(WORD_DELIMITER));
-                List<WordInfo> wordInfos = new ArrayList<>();
-
-                Set<String> uniqueWords = new HashSet<>(words);
-                for (String uniqueWord:uniqueWords) {
-                    int count = (int) words.stream().filter(word->{
-                        return word.equals(uniqueWord);
-                    }).count();
-                    wordInfos.add(new WordInfo(uniqueWord,count));
-                }
-
-                wordInfos.sort((fistWord, secondWord) -> secondWord.getWordCount() - fistWord.getWordCount());
-
-                StringJoiner joiner = new StringJoiner(LINE_DELIMITER);
-                for (WordInfo wordInfo : wordInfos) {
-                    String wordCoutString = wordInfo.getWord() + SPACE_DELIMITER + wordInfo.getWordCount();
-                    joiner.add(wordCoutString);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-                return CALCULATE_ERROR;
+            Set<String> uniqueWords = new HashSet<>(words);
+            for (String uniqueWord : uniqueWords) {
+                int count = (int) words.stream().filter(word -> {
+                    return word.equals(uniqueWord);
+                }).count();
+                wordInfos.add(new WordInfo(uniqueWord, count));
             }
+
+            wordInfos.sort((fistWord, secondWord) -> secondWord.getWordCount() - fistWord.getWordCount());
+
+            StringJoiner joiner = new StringJoiner(LINE_DELIMITER);
+            for (WordInfo wordInfo : wordInfos) {
+                String wordCoutString = wordInfo.getWord() + SPACE_DELIMITER + wordInfo.getWordCount();
+                joiner.add(wordCoutString);
+            }
+            return joiner.toString();
+        } catch (Exception e) {
+            return CALCULATE_ERROR;
         }
     }
 }
